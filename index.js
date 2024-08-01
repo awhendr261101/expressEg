e6b91644d7399a9378990a595ae3fc04208c862a
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-01 12:38:13 +0200

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// hold on to this
+import express from 'express';
+import {resolve, dirname} from 'path';
+
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(express.static('./static'))
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(resolve('./static/index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).sendFile(resolve('./static/about.html'));
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
